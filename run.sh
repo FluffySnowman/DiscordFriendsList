0f46c16c67dc541f76d844a1c7c5319859c963c9
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-27 14:47:23 +0530

    works

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 echo "Welcome to the discord friends list extractor"
 echo "Keep your discord data package ready for this"
+echo ""
+echo "MOVE/COPY YOUR user.json FILE TO THIS FOLDER. PROGRAM WILL NOT WORK IF user.json IS NOT IN THIS FOLDER"
+echo "user.json IS LOCATED IN THE account FOLDER THAT IS LOCATED INSIDE THE .zip FILE THAT YOU DOWNLOAD"
 read -p "Press enter to install required packages" null
-#sudo apt install jq
+sudo apt install jq
 read -p "Press [ENTER] to start program" null
 
 cat user.json | jq '.relationships[] | {user}' > friends.json
