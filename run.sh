062da35ed5a7bebd07e8ee165de78d109ff48429
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-27 14:31:57 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+echo "Welcome to the discord friends list extractor"
+echo "Keep your discord data package ready for this"
+read -p "Press enter to install required packages" null
+sudo apt install jq
+read -p "Press [ENTER] to start program" null
+read -p "Enter path to user.json>>> " $ddp_user_path
+
+
