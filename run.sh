2de4f37c3993100b90fa49beb7c48a7232b41e27
fluffy <thefluffysnowmanyt@gmail.com>
2022-05-27 14:44:31 +0530

    trying something

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 echo "Welcome to the discord friends list extractor"
 echo "Keep your discord data package ready for this"
 read -p "Press enter to install required packages" null
-sudo apt install jq
+#sudo apt install jq
 read -p "Press [ENTER] to start program" null
-read -p "Enter path to user.json>>> " $ddp_user_path
 
+cat user.json | jq '.relationships[] | {user}' > friends.json
+
+echo "Friends list has been saved to this folder with a .json format"
+echo "Thanks for using this program. Make sure to check out my other programs on https://github.com/FluffySnowman"
 
